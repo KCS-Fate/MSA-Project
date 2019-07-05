0fd408bf7135b1e38ea5d3fbf145e19e181d2a37
KCS-Fate <kimchangsern@yahoo.com>
2019-07-05 20:54:11 +1200

    additional file
    API source code

---
+++ schoolSIMS/Model/schoolSIMSContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace schoolSIMS.Model
+{
+    public partial class schoolSIMSContext : DbContext
+    {
+        public schoolSIMSContext()
+        {
+        }
+
+        public schoolSIMSContext(DbContextOptions<schoolSIMSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=tcp:msaschooldatabase.database.windows.net,1433;Initial Catalog=schoolSIMS;Persist Security Info=False;User ID=Tester;Password=Tester123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber).IsUnicode(false);
+
+                entity.Property(e => e.Postaladdress).IsUnicode(false);
+            });
+        }
+    }
+}
